7b245f60a961cd88ee58247844794d89cd84876e
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2022-09-19 10:52:20 +0000

    Fixed specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'bundler/setup'
-require 'sidekiq_heroku_scaler'
+require 'sidekiq-heroku-scaler'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
